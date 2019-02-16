4946fff6ff9c2fe4f81786947b25543b4658b311
jjwdowik <jjwdowik@umich.edu>
2019-02-16 17:24:52 -0500

    add google recaptcha example file

--- src/pages/contact/examples.js
+++ src/pages/contact/examples.js
@@ -16,6 +16,7 @@ export default class Index extends React.Component {
               <ul>
                 <li><Link to="/contact">Basic contact form</Link></li>
                 <li><Link to="/contact/file-upload/">Form with file upload</Link></li>
+                <li><Link to="/contact/recaptcha/">reCAPTCHA</Link></li>
               </ul>
 
               <h2>Troubleshooting</h2>
