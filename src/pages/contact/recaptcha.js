9da468d35dcbad9a9462b05e9070c2ccbaee8574
jjwdowik <jjwdowik@umich.edu>
2019-02-16 18:47:41 -0500

    better layout

--- src/pages/contact/recaptcha.js
+++ src/pages/contact/recaptcha.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { navigateTo } from "gatsby-link";
 import Recaptcha from "react-google-recaptcha";
+import Layout from '../../components/Layout'
 
 const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY;
 
@@ -18,11 +19,11 @@ export default class Contact extends React.Component {
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
   handleRecaptcha = value => {
     this.setState({ "g-recaptcha-response": value });
-  }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
@@ -44,47 +45,54 @@ export default class Contact extends React.Component {
     console.log(process.env)
     console.log(RECAPTCHA_KEY)
     return (
-      <div>
-        <h1>reCAPTCHA 2</h1>
-        <form
-          name="contact-recaptcha"
-          method="post"
-          action="/contact/thanks/"
-          data-netlify="true"
-          data-netlify-recaptcha="true"
-          onSubmit={this.handleSubmit}
-        >
-          <noscript>
-            <p>This form won’t work with Javascript disabled</p>
-          </noscript>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <Recaptcha
-            ref="recaptcha"
-            sitekey={RECAPTCHA_KEY}
-            onChange={this.handleRecaptcha}
-          />
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
+      <Layout>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <h1>reCAPTCHA 2</h1>
+              <form
+                name="contact-recaptcha"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify-recaptcha="true"
+                data-netlify="true"
+                onSubmit={this.handleSubmit}
+              >
+                <noscript>
+                  <p>This form won’t work with Javascript disabled</p>
+                </noscript>
+                <p>
+                  <label>
+                    Your name:<br />
+                    <input type="text" name="name" onChange={this.handleChange} required={true} />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Your email:<br />
+                    <input type="email" name="email" onChange={this.handleChange} required={true} />
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    Message:<br />
+                    <textarea name="message" onChange={this.handleChange} required={true} />
+                  </label>
+                </p>
+                <Recaptcha
+                  ref="recaptcha"
+                  sitekey={RECAPTCHA_KEY}
+                  onChange={this.handleRecaptcha}
+                  required={true}
+                />
+                <p>
+                  <button type="submit">Send</button>
+                </p>
+              </form>
+            </div>
+          </div>
+        </section>
+      </Layout>
     );
   }
 }
